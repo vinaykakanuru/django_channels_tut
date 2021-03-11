ff74aad43d5a75e4aad47941d14f4469c2d1267e
K VINAY KUMAR REDDY <kvinaykumarreddy1995@gmail.com>
2021-03-11 12:37:58 +0530

    Initial commit

---
+++ chat/consumers.py
@@ -0,0 +1,53 @@
+import json
+# if our consumer is synchronous and we want to make async then we should use async_to_sync
+# from asgiref.sync import async_to_sync
+# if we want to use our consumer as sync then we need to inherit WebSockerConsumer
+# from channels.generic.websocket import WebsocketConsumer
+
+# if we want to use our consumer as async then we need to inherit WebSAsyncWebsocketConsumerockerConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = 'chat_%s' % self.room_name
+
+        # Join room group
+        # await is used to call asynchronous functions that perform I/O.
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        # Leave room group
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    # Receive message from WebSocket
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+
+        # Send message to room group
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message
+            }
+        )
+
+    # Receive message from room group
+    async def chat_message(self, event):
+        message = event['message']
+
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps({
+            'message': message
+        }))
